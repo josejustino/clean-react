fec24fed25a0303bb581f522a59de30fcd2b26aa
josejustino <josejustinno@gmail.com>
2023-06-24 21:44:36 -0300

    feat: add authentication usecase

---
+++ src/domain/usecases/authentication.ts
@@ -0,0 +1,10 @@
+import { type AccountModel } from '../models/account-model'
+
+type AuthenticationParams = {
+  email: string
+  password: string
+}
+
+export interface Authentication {
+  auth: (params: AuthenticationParams) => Promise<AccountModel>
+}
