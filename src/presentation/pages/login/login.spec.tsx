fac8f89b1ebeca6f56a4f22bc37563f6966965da
josejustino <josejustinno@gmail.com>
2023-06-28 15:03:07 -0300

    test: ensure Login will not load spinner and error on start

---
+++ src/presentation/pages/login/login.spec.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { screen, render } from '@testing-library/react'
+
+import { Login } from '@/presentation/pages'
+
+describe('Login Component', () => {
+  test('Should not render spinner and error on start', () => {
+    render(<Login />)
+
+    const errorWrap = screen.getByTestId('error-wrap')
+
+    expect(errorWrap.childElementCount).toBe(0)
+  })
+})
