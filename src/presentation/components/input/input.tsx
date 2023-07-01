916deb86273c906b89023bb09461843c5adfcbb8
josejustino <josejustinno@gmail.com>
2023-07-01 13:42:16 -0300

    feat: ensure Login present valid state if password is valid

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -22,11 +22,11 @@ const Input: React.FC<Props> = (props) => {
   }
 
   const getStatus = (): string => {
-    return '🔴'
+    return error ? '🔴' : '🟢'
   }
 
   const getTitle = (): string => {
-    return error
+    return error || 'Tudo certo!'
   }
 
   return (
