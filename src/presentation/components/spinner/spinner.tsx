a6181792d0b311aff9c2056d2fae4fad5709220d
josejustino <josejustinno@gmail.com>
2023-07-02 11:24:00 -0300

    feat: ensute Login present spinner on submit

--- src/presentation/components/spinner/spinner.tsx
+++ src/presentation/components/spinner/spinner.tsx
@@ -6,7 +6,7 @@ type Props = React.HTMLAttributes<HTMLElement>
 
 const Spinner: React.FC<Props> = (props) => {
   return (
-    <div {...props} className={[Styles.spinner, props.className].join(' ')}>
+    <div {...props} data-testid="spinner" className={[Styles.spinner, props.className].join(' ')}>
       <div /><div /><div /><div />
     </div>
   )
