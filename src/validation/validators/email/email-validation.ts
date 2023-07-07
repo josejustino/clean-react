529b94491d1e45196fb4a8c7399a6ba6755d86a8
josejustino <josejustinno@gmail.com>
2023-07-07 20:01:54 -0300

    feat: ensure e-mail validation returns error if e-mail is invalid

---
+++ src/validation/validators/email/email-validation.ts
@@ -0,0 +1,9 @@
+import { InvalidFieldError } from '@/validation/errors/invalid-field-error'
+import { type FieldValidation } from '@/validation/protocols/field-validation'
+
+export class EmailValidation implements FieldValidation {
+  constructor (readonly field: string) {}
+  validate (value: string): Error {
+    return new InvalidFieldError()
+  }
+}
