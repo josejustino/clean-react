520cff92f3a37abb654033b9a7a20eebee26cd49
josejustino <josejustinno@gmail.com>
2023-07-08 16:20:00 -0300

    feat: add env vars to API URL

--- jest.config.js
+++ jest.config.js
@@ -4,9 +4,7 @@ module.exports = {
     '<rootDir>/src/**/*.{ts,tsx}',
     '!<rootDir>/src/main/**/*',
     '!<rootDir>/src/presentation/components/router/**/*',
-    '!<rootDir>/src/domain/models/index.ts',
-    '!<rootDir>/src/domain/usecases/index.ts',
-    '!<rootDir>/src/presentation/components/index.ts',
+    '!<rootDir>/src/**/index.ts',
     '!**/*.d.ts'
   ],
   coverageDirectory: 'coverage',
