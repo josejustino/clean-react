9b238ca04a98e1beb04fbf08fa03182f5ea3a659
josejustino <josejustinno@gmail.com>
2023-07-07 23:06:55 -0300

    test: ensure ValidationBuilder returns a list of validations

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,21 +1,38 @@
 import { EmailValidation, MinLengthValidation, RequiredFieldValidation, ValidationBuilder as sut } from '@/validation/validators'
+import { faker } from '@faker-js/faker'
 
 describe('ValidationBuilder', () => {
   test('Should return RequiredFieldValidation', () => {
-    const validations = sut.field('any_field').required().build()
+    const field = faker.database.column()
+    const validations = sut.field(field).required().build()
 
-    expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+    expect(validations).toEqual([new RequiredFieldValidation(field)])
   })
 
   test('Should return EmailValidation', () => {
-    const validations = sut.field('any_field').email().build()
+    const field = faker.database.column()
+    const validations = sut.field(field).email().build()
 
-    expect(validations).toEqual([new EmailValidation('any_field')])
+    expect(validations).toEqual([new EmailValidation(field)])
   })
 
   test('Should return MinLengthValidation', () => {
-    const validations = sut.field('any_field').min(5).build()
+    const length = faker.number.int()
+    const field = faker.database.column()
+    const validations = sut.field(field).min(length).build()
 
-    expect(validations).toEqual([new MinLengthValidation('any_field', 5)])
+    expect(validations).toEqual([new MinLengthValidation(field, length)])
+  })
+
+  test('Should return a list of validations', () => {
+    const length = faker.number.int()
+    const field = faker.database.column()
+    const validations = sut.field(field).required().min(length).email().build()
+
+    expect(validations).toEqual([
+      new RequiredFieldValidation(field),
+      new MinLengthValidation(field, length),
+      new EmailValidation(field)
+    ])
   })
 })
