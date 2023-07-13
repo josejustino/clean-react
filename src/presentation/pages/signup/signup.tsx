e90389c41df39c8e8fb30162fd97d44ee3167793
josejustino <josejustinno@gmail.com>
2023-07-13 17:55:05 -0300

    feat: ensure SignUp prevent submit if form is invalid

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -43,7 +43,12 @@ const SignUp: React.FC<Props> = ({ validation, addAccount }) => {
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault()
 
-    if (state.isLoading) {
+    if (state.isLoading ||
+        state.nameError ||
+        state.emailError ||
+        state.passwordError ||
+        state.passwordConfirmationError
+    ) {
       return
     }
 
