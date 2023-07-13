30b878bb401362a8deb21feff0b70afd61e3e274
josejustino <josejustinno@gmail.com>
2023-07-13 18:42:57 -0300

    test: ensure Login link go to right place

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -232,4 +232,15 @@ describe('SignUp Component', () => {
     await Helper.testElementText(sut, 'main-error', error.message)
     Helper.testChildCount(sut, 'error-wrap', 1)
   })
+
+  test('Should go to login page', async () => {
+    const { sut } = makeSut()
+
+    const loginLink = sut.getByTestId('login-link')
+
+    fireEvent.click(loginLink)
+
+    expect(window.history.length).toBe(1)
+    expect(history.location.pathname).toBe('/login')
+  })
 })
