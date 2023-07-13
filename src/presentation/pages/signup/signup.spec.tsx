594d38354dfdfe7022420763ca3a81917cc1daef
josejustino <josejustinno@gmail.com>
2023-07-13 13:43:44 -0300

    feat: ensure SignUp show valid email if validation succeeds

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -82,4 +82,12 @@ describe('SignUp Component', () => {
 
     Helper.testStatusForField(sut, 'name')
   })
+
+  test('Should show valid email state if Validation succeeds', () => {
+    const { sut } = makeSut()
+
+    Helper.populateField(sut, 'email')
+
+    Helper.testStatusForField(sut, 'email')
+  })
 })
