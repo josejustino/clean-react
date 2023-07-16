e5aebc28296a4ae248340925e307b3a1aac5163f
josejustino <josejustinno@gmail.com>
2023-07-15 22:54:23 -0300

    chore: config cypress and add first command

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,40 +1,13 @@
 import { defineConfig } from "cypress";
-import webpackPreprocessor from "@cypress/webpack-preprocessor";
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      const options = {
-        resolve: {
-          extensions: ['.ts', '.js',],
-        },
-        webpackOptions: {
-          module: {
-            rules: [
-              {
-                test: /\.ts?$/,
-                exclude: /node_modules/,
-                loader: 'ts-loader',
-              },
-            ],
-          },
-        }
-      };
-      on("file:preprocessor", webpackPreprocessor(options));
     },
-    baseUrl: "http://localhost:3000",
+    baseUrl: 'http://localhost:3000',
     fixturesFolder: false,
-    supportFile: false,
-    fileServerFolder: "src/main/test/cypress",
-    downloadsFolder: "src/main/test/cypress/downloads",
-    specPattern: 'src/main/test/cypress/e2e/**/*.spec.ts'
-  },
-
-  component: {
-    devServer: {
-      framework: "react",
-      bundler: "webpack",
-    },
+    supportFile: 'src/main/test/cypress/support/index.js',
+    specPattern: 'src/main/test/cypress/e2e/**/*.spec.ts',
   },
 });
