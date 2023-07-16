0da522688393d00f5753073c5accb8179cdfa34b
josejustino <josejustinno@gmail.com>
2023-07-16 10:26:37 -0300

    test: ensure Login presents error state if form is invalid

--- src/main/tests/cypress/e2e/login.spec.ts
+++ src/main/tests/cypress/e2e/login.spec.ts
@@ -1,13 +1,34 @@
+import { faker } from '@faker-js/faker'
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login')
   })
 
   it('Should load with correct initial state', () => {
+    cy.getByTestId('email').should('have.attr', 'readonly')
     cy.getByTestId('email-status')
       .should('have.attr', 'title', 'Campo obrigatório')
       .should('contain.text', '🔴')
+
+    cy.getByTestId('password').should('have.attr', 'readonly')
     cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', '🔴')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('Should present error state if form is invalid', () => {
+    cy.getByTestId('email').focus()
+    cy.getByTestId('email').type(faker.word.words())
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Valor inválido')
+      .should('contain.text', '🔴')
+
+    cy.getByTestId('password').focus()
+    cy.getByTestId('password').type(faker.string.alphanumeric({ length: 3 }))
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Valor inválido')
       .should('contain.text', '🔴')
     cy.getByTestId('submit').should('have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
