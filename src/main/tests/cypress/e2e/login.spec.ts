741778402af2ed98c4d5d8550e371e6bbc78b92b
josejustino <josejustinno@gmail.com>
2023-07-23 11:08:23 -0300

    refactor: move login-mocks to support folder

--- src/main/tests/cypress/e2e/login.spec.ts
+++ src/main/tests/cypress/e2e/login.spec.ts
@@ -1,7 +1,7 @@
 import { faker } from '@faker-js/faker'
 
 import * as FormHelper from '../support/form-helper'
-import * as Http from './login-mocks'
+import * as Http from '../support/login-mocks'
 
 const simulateValidSubmit = (): void => {
   cy.getByTestId('email').focus()
