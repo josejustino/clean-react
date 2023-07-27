3a542ec113ff47f3f32d434210312f381003938b
josejustino <josejustinno@gmail.com>
2023-07-27 09:51:01 -0300

    feat: add skeleton shimmer effect

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -22,31 +22,9 @@ const SurveyList: React.FC = () => {
           <footer>Ver Resultado</footer>
         </li>
 
-        <li>
-          <div className={Styles.surveyContent}>
-           <Icon className={Styles.iconWrap} iconName={IconName.thumbUp} />
-            <time>
-              <span className={Styles.day}>25</span>
-              <span className={Styles.month}>07</span>
-              <span className={Styles.year}>2023</span>
-            </time>
-            <p>Qual é seu framework web favorito?</p>
-          </div>
-          <footer>Ver Resultado</footer>
-        </li>
-
-        <li>
-          <div className={Styles.surveyContent}>
-           <Icon className={Styles.iconWrap} iconName={IconName.thumbUp} />
-            <time>
-              <span className={Styles.day}>25</span>
-              <span className={Styles.month}>07</span>
-              <span className={Styles.year}>2023</span>
-            </time>
-            <p>Qual é seu framework web favorito?</p>
-          </div>
-          <footer>Ver Resultado</footer>
-        </li>
+        <li></li>
+        <li></li>
+        <li></li>
       </ul>
     </div>
     <Footer />
