b590097f6b6ccd09febe731f9cca9e4112fa1bc9
josejustino <josejustinno@gmail.com>
2023-07-29 14:45:18 -0300

    feat: ensure PrivateRoute render component if token is not empty

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -2,15 +2,17 @@ import React, { useMemo } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import { ApiContext } from '@/presentation/contexts'
-import { setCurrentAccountAdapter } from '@/main/adapters/current-account-adapter'
+import { getCurrentAccountAdapter, setCurrentAccountAdapter } from '@/main/adapters/current-account-adapter'
 
 import { MakeLogin } from '@/main/factories/pages/login/login-factory'
 import { MakeSignUp } from '@/main/factories/pages/signup/signup-factory'
 import { SurveyList } from '@/presentation/pages'
+import { PrivateRoute } from '@/presentation/components'
 
 const Router: React.FC = () => {
   const context = useMemo(() => ({
-    setCurrentAccount: setCurrentAccountAdapter
+    setCurrentAccount: setCurrentAccountAdapter,
+    getCurrentAccount: getCurrentAccountAdapter
   }), [])
 
   return (
@@ -21,7 +23,12 @@ const Router: React.FC = () => {
         <Routes>
           <Route path='/login' element={<MakeLogin />} />
           <Route path='/signup' element={<MakeSignUp />} />
-          <Route path='/' element={<SurveyList />} />
+
+          <Route
+            element={<PrivateRoute />}
+          >
+            <Route path='/' element={<SurveyList />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </ApiContext.Provider>
