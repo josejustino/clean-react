8913699ac1934facdab93df74807be73b0ce9a36
josejustino <josejustinno@gmail.com>
2023-08-06 13:26:06 -0300

    feat: ensure AuthorizeHttpGetClientDecorator calls GetStorage with correct value

---
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
@@ -0,0 +1,11 @@
+import { type GetStorage } from '@/data/protocols/cache'
+import { type HttpGetClient, type HttpGetParams, type HttpResponse } from '@/data/protocols/http'
+
+export class AuthorizeHttpGetClientDecorator implements HttpGetClient {
+  constructor (private readonly getStorage: GetStorage) {}
+
+  async get (params: HttpGetParams): Promise<HttpResponse> {
+    this.getStorage.get('account')
+    return null
+  }
+}
