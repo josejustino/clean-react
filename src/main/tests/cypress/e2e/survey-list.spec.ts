dabe743e84d0e3efd7bdde401e840c19aaee3e85
josejustino <josejustinno@gmail.com>
2023-08-15 21:16:16 -0300

    test: ensure SurveyList logout on logout link click

--- src/main/tests/cypress/e2e/survey-list.spec.ts
+++ src/main/tests/cypress/e2e/survey-list.spec.ts
@@ -27,4 +27,12 @@ describe('SurveyList', () => {
     const { name } = Helper.getLocalStorageItem('account')
     cy.getByTestId('username').should('contain.text', name)
   })
+
+  it('Should logout on logout link click', () => {
+    Http.mockUnexpectedError()
+
+    cy.getByTestId('logout').click()
+
+    Helper.testUrl('/login')
+  })
 })
