3148f8c0e76eedd639ea0e60a641f9680e5f1fa1
josejustino <josejustinno@gmail.com>
2023-09-01 21:46:32 -0300

    fix: change fields names to not use faker

--- src/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -7,8 +7,8 @@ const makeSut = (field: string, fieldToCompare: string): CompareFieldsValidation
 
 describe('CompareFieldsValidation', () => {
   test('Should return error if compare is invalid', () => {
-    const field = faker.database.column()
-    const fieldToCompare = faker.database.column()
+    const field = 'field_1'
+    const fieldToCompare = 'field_2'
     const sut = makeSut(field, fieldToCompare)
     const error = sut.validate({
       [field]: faker.word.words(3),
