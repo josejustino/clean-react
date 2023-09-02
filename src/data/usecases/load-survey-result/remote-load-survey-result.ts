cefb4a950b67c43c19c2e4b451e100a075b57a60
josejustino <josejustinno@gmail.com>
2023-09-02 11:04:30 -0300

    feat: ensure RemoteLoadSurveyResult calls HttpGetClient with correct URL

---
+++ src/data/usecases/load-survey-result/remote-load-survey-result.ts
@@ -0,0 +1,12 @@
+import { type HttpGetClient } from '@/data/protocols/http'
+
+export class RemoveLoadSurveyResult {
+  constructor (
+    private readonly url: string,
+    private readonly httpGetClient: HttpGetClient
+  ) {}
+
+  async load (): Promise<void> {
+    await this.httpGetClient.get({ url: this.url })
+  }
+}
