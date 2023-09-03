004740cc9a57bbfcc392400b1f8db25e5922b70f
josejustino <josejustinno@gmail.com>
2023-09-03 10:52:42 -0300

    test: ensure RemoveLoadSurveyResult throws UnexpectedError on 500

--- src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
@@ -50,4 +50,15 @@ describe('RemoveLoadSurveyResult', () => {
 
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
+
+  test('Should throw UnexpectedError if HttpGetClient return 500', async () => {
+    const { sut, httpGetClientSpy } = makeSut()
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.serverError
+    }
+
+    const promise = sut.load()
+
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
 })
