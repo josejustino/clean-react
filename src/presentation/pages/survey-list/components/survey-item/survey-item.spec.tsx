aa214fc7887024920f8ee496508373dbbc773377
josejustino <josejustinno@gmail.com>
2023-09-03 13:57:52 -0300

    refactor: move Calendar to its on Component

--- src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
@@ -11,31 +11,23 @@ const makeSut = (survey = mockSurveyModel()): void => {
 describe('SurveyItem Component', () => {
   test('Should render with correct values', () => {
     const survey = Object.assign(mockSurveyModel(), {
-      didAnswer: true,
-      date: new Date('2023-07-30T00:00:00')
+      didAnswer: true
     })
 
     makeSut(survey)
 
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbUp)
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
-    expect(screen.getByTestId('day')).toHaveTextContent('30')
-    expect(screen.getByTestId('month')).toHaveTextContent(/^jul$/)
-    expect(screen.getByTestId('year')).toHaveTextContent('2023')
   })
 
   test('Should render with correct values', () => {
     const survey = Object.assign(mockSurveyModel(), {
-      didAnswer: false,
-      date: new Date('2023-06-03T00:00:00')
+      didAnswer: false
     })
 
     makeSut(survey)
 
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbDown)
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
-    expect(screen.getByTestId('day')).toHaveTextContent('03')
-    expect(screen.getByTestId('month')).toHaveTextContent(/^jun$/)
-    expect(screen.getByTestId('year')).toHaveTextContent('2023')
   })
 })
