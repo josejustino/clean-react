05df72fd8a39db348265dbc22a86606759243a60
josejustino <josejustinno@gmail.com>
2023-09-03 10:49:47 -0300

    test: ensure RemoveLoadSurveyResult throws AccessDeniedError on 403

--- src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
@@ -1,6 +1,8 @@
 import { faker } from '@faker-js/faker'
 import { RemoveLoadSurveyResult } from '@/data/usecases'
 import { HttpGetClientSpy } from '@/data/test'
+import { HttpStatusCode } from '@/data/protocols/http'
+import { AccessDeniedError } from '@/domain/errors'
 
 type SutTypes = {
   sut: RemoveLoadSurveyResult
@@ -25,5 +27,16 @@ describe('RemoveLoadSurveyResult', () => {
     await sut.load()
 
     expect(httpGetClientSpy.url).toBe(url)
+  })
+
+  test('Should throw AccessDeniedError if HttpGetClient return 403', async () => {
+    const { sut, httpGetClientSpy } = makeSut()
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.forbidden
+    }
+
+    const promise = sut.load()
+
+    await expect(promise).rejects.toThrow(new AccessDeniedError())
   })
 })
