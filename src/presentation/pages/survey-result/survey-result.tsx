54365ccc47e8cb7ae38d68ced0588203bec68fdf
josejustino <josejustinno@gmail.com>
2023-09-03 21:11:53 -0300

    feat: add skeleton with shimmer on SurveyResult

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -9,32 +9,36 @@ const SurveyResult: React.FC = () => {
     <div className={Styles.surveyResultWrap}>
       <Header />
       <div className={Styles.contentWrap}>
-        <hgroup>
-          <Calendar date={new Date()} className={Styles.calendarWrap} />
-        <h2>Qual é seu framework web favorito?</h2>
-        </hgroup>
-        <FlipMove className={Styles.anwersList}>
-          <li>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/360px-React-icon.svg.png" alt="" />
-            <span className={Styles.anwer}>ReactJS</span>
-            <span className={Styles.percent}>50%</span>
-          </li>
+        {false && (
+          <>
+            <hgroup>
+              <Calendar date={new Date()} className={Styles.calendarWrap} />
+              <h2>Qual é seu framework web favorito? Qual é seu framework web favorito?</h2>
+            </hgroup>
+            <FlipMove className={Styles.anwersList}>
+              <li>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/360px-React-icon.svg.png" alt="" />
+                <span className={Styles.answer}>ReactJS</span>
+                <span className={Styles.percent}>50%</span>
+              </li>
 
-          <li className={Styles.active}>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/360px-React-icon.svg.png" alt="" />
-            <span className={Styles.anwer}>VueJS</span>
-            <span className={Styles.percent}>30%</span>
-          </li>
+              <li className={Styles.active}>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/360px-React-icon.svg.png" alt="" />
+                <span className={Styles.answer}>VueJS</span>
+                <span className={Styles.percent}>30%</span>
+              </li>
 
-          <li>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/360px-React-icon.svg.png" alt="" />
-            <span className={Styles.anwer}>AngularJS</span>
-            <span className={Styles.percent}>20%</span>
-          </li>
-        </FlipMove>
-        <button>Voltar</button>
+              <li>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/360px-React-icon.svg.png" alt="" />
+                <span className={Styles.answer}>AngularJS</span>
+                <span className={Styles.percent}>20%</span>
+              </li>
+            </FlipMove>
+            <button>Voltar</button>
+          </>
+        )}
 
-       {false && <Loading />}
+        {false && <Loading />}
       </div>
       <Footer />
     </div>
