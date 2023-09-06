f553623ef1f1097560b6b61b16d6b650ce3ab7bb
josejustino <josejustinno@gmail.com>
2023-09-06 17:33:26 -0300

    refactor: move SurveyResultAnswer to its own Component

--- src/presentation/pages/survey-result/components/index.ts
+++ src/presentation/pages/survey-result/components/index.ts
@@ -1,1 +1,2 @@
 export { default as SurveyResultData } from './survey-result-data/survey-result-data'
+export { default as SurveyResultAnswer } from './survey-result-answer/survey-result-answer'
--- src/presentation/pages/survey-result/components/survey-result-data/survey-result-data.tsx
+++ src/presentation/pages/survey-result/components/survey-result-data/survey-result-data.tsx
@@ -5,8 +5,9 @@ import FlipMove from 'react-flip-move'
 import { type LoadSurveyResult } from '@/domain/usecases'
 
 import { Calendar } from '@/presentation/components'
+import { SurveyResultAnswer } from '@/presentation/pages/survey-result/components'
 
-import Styles from './result-styles.scss'
+import Styles from './survey-result-data-styles.scss'
 
 type Props = {
   surveyResult: LoadSurveyResult.Model
@@ -22,13 +23,9 @@ const SurveyResultData: React.FC<Props> = ({ surveyResult }) => {
         <h2 data-testid="question">{surveyResult.question}</h2>
       </hgroup>
       <FlipMove data-testid="answers" className={Styles.answersList}>
-        {surveyResult.answers.map(answer => (
-          <li data-testid="answer-wrap" key={answer.answer} className={answer.isCurrentAccountAnswer ? Styles.active : ''}>
-            {answer.image && <img data-testid="image" src={answer.image} alt={answer.answer} />}
-            <span data-testid="answer" className={Styles.answer}>{answer.answer}</span>
-            <span data-testid="percent" className={Styles.percent}>{answer.percent}%</span>
-          </li>
-        ))}
+        <>
+          {surveyResult.answers.map(answer => <SurveyResultAnswer key={answer.answer} answer={answer} />)}
+        </>
       </FlipMove>
       <button className={Styles.button} data-testid="back-button" onClick={() => { navigate('/') }}>Voltar</button>
     </>
