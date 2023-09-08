573d1f02ff7f6e4a0c52b90377071efe332e1d33
josejustino <josejustinno@gmail.com>
2023-09-08 11:28:00 -0300

    test: ensure SurveyResult present new result on answer click

--- src/main/tests/cypress/e2e/survey-result.spec.ts
+++ src/main/tests/cypress/e2e/survey-result.spec.ts
@@ -2,7 +2,7 @@ import * as Helper from '../utils/helpers'
 import * as Http from '../utils/http-mocks'
 
 const path = 'surveys/*/results'
-export const mockLoadSuccess = (): void => { Http.mockOk(path, 'GET', 'survey-result') }
+export const mockLoadSuccess = (): void => { Http.mockOk(path, 'GET', 'load-survey-result') }
 
 describe('SurveyResult', () => {
   describe('load', () => {
@@ -74,6 +74,7 @@ describe('SurveyResult', () => {
   describe('save', () => {
     const mockUnexpectedError = (): void => { Http.mockServerError(path, 'PUT') }
     const mockAccessDeniedError = (): void => { Http.mockForbiddenError(path, 'PUT') }
+    const mockSaveSuccess = (): void => { Http.mockOk(path, 'PUT', 'save-survey-result') }
 
     beforeEach(() => {
       cy.fixture('account').then(account => {
@@ -95,6 +96,26 @@ describe('SurveyResult', () => {
       cy.get('li:nth-child(2)').click()
 
       Helper.testUrl('/login')
+    })
+
+    it('Should present survey result', () => {
+      mockSaveSuccess()
+
+      cy.get('li:nth-child(2)').click()
+      cy.getByTestId('question').should('have.text', 'Other Question')
+      cy.getByTestId('day').should('have.text', '23')
+      cy.getByTestId('month').should('have.text', 'mar')
+      cy.getByTestId('year').should('have.text', '2020')
+      cy.get('li:nth-child(1)').then(li => {
+        assert.equal(li.find('[data-testid="answer"]').text(), 'other_answer')
+        assert.equal(li.find('[data-testid="percent"]').text(), '50%')
+        assert.equal(li.find('[data-testid="image"]').attr('src'), 'other_image')
+      }).should('have.css', 'box-shadow', 'rgb(188, 71, 123) 0px 0px 3px 2px')
+      cy.get('li:nth-child(2)').then(li => {
+        assert.equal(li.find('[data-testid="answer"]').text(), 'other_answer_2')
+        assert.equal(li.find('[data-testid="percent"]').text(), '50%')
+        assert.notExists(li.find('[data-testid="image"]'))
+      }).should('not.have.css', 'box-shadow', 'rgb(188, 71, 123) 0px 0px 3px 2px')
     })
   })
 })
